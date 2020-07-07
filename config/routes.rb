0333492358302043332a506f5a0204fe3a4bcd1b
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-07-07 10:33:09 -0400

    reconfiguring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :entries
-  resources :journals
+  root 'journals#index'
+  resources :journals do
+    resources :entries, shallow: true
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
