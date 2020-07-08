4dbdaad4d6178fda41d6fe2da3735718bc91a730
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-07-08 13:06:20 -0400

    trying to fix name validation

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -42,12 +42,12 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :name, :password)}
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
    end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :name, :password)}
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
    end
 
   # The path used after sign up.
