de320a472164ca60f33bcf1bd74f35e645570990
Alida Weatherbee <alida.weatherbee@shopify.com>
2020-07-08 10:02:53 -0400

    wtf bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :journals do
-    resources :entries, shallow: true
+    resources :entries
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
